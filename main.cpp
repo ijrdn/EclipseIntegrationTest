a08e918e3d870fd9dbb1d84bd084031f84497c17
ijrdn <ijrdn@ijrdn-PC.gateway.2wire.net>
2014-07-11 23:25:09 -0400

    Hello World!

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: ijrdn
+ *
+ * Created on July 11, 2014, 11:23 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
+
